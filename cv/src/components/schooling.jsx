c9e38c3665fc806f4818977a2ccb40ed79215837
Amgad1123 <Amgadahmed2303@gmail.com>
2025-09-03 09:51:17 -0500

    Added save and cancel buttons to the Education div.

--- cv/src/components/schooling.jsx
+++ cv/src/components/schooling.jsx
@@ -7,25 +7,33 @@ export default function SchoolInfo ({schoolName, setSchoolName, studyName, setSt
             e.preventDefault();
 
         }}>
-            <div class = "input-col"> 
-                <label htmlFor="school">School/University*</label>
-                <input value = {schoolName} type="text" id = "school" onChange = {e => setSchoolName(e.target.value)}/>
-            </div>
-            <div class = "input-col"> 
-                <label htmlFor="studyTitle">Study Title:</label>
-                <input type="text" id= "studyTitle" value={studyName} onChange = {e => setStudyName(e.target.value)}/>
-            </div>
-            <div class = "input-row-dates">
-                <div className="input-col">
-                    <label htmlFor="startDate">Start Date: </label>
-                    <input type="date" id="startDate"/>
+                <button></button>
+                <div className="school-input">
+                    <div class = "input-col"> 
+                        <label htmlFor="school">School/University*</label>
+                        <input value = {schoolName} type="text" id = "school" onChange = {e => setSchoolName(e.target.value)}/>
+                    </div>
+                    <div class = "input-col"> 
+                        <label htmlFor="studyTitle">Study Title:</label>
+                        <input type="text" id= "studyTitle" value={studyName} onChange = {e => setStudyName(e.target.value)}/>
+                    </div>
+                    <div class = "input-row-dates">
+                        <div className="input-col">
+                            <label htmlFor="startDate">Start Date: </label>
+                            <input type="date" id="startDate"/>
+                        </div>
+                        <div className="input-col">
+                            <label htmlFor="endDate">End Date: </label>
+                            <input type="date" id="endDate"/>
+                        </div>
+                    </div>
+
                 </div>
-                <div className="input-col">
-                    <label htmlFor="endDate">End Date: </label>
-                    <input type="date" id="endDate"/>
+                <div className="button-row">
+                    <button className="save">Save</button>
+                    <button class="cancel">Cancel</button>
                 </div>
-            </div>
-            </form>
+            </form>       
         </div>
     )
 }
